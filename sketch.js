0a0db6f29b04eb22069c7d9af290e1a91c33083a
oihv <kokolas16@gmail.com>
2025-03-24 21:00:09 +0800

    Adjust main js with robot class

--- sketch.js
+++ sketch.js
@@ -1,41 +1,63 @@
 const canvasWidth = 1575; // Scale 1:2.67
 const canvasHeight = 1200;
-
-function setup() {
-    window.canvas = createCanvas(canvasWidth, canvasHeight); 
-    window.canvas.parent("canvas");
-}
-
 const wallWidth = 37.45;
 const verticalLineGap = 281;
 const horizontalLineGap = 250;
 const highLookoutWidth = 150;
-function draw() {
-  background('#1b263b');
-  
-  stroke('#e0e1dd');
-  strokeWeight(5);
-  fill('#778da9')
-  // Vertical lines
-  for (var i = 0 ; i < 7 ; i++) {
-    line(wallWidth + (i*horizontalLineGap), 0, wallWidth + (i*horizontalLineGap), canvasHeight);
-    // Lookout rectangle
-    if (i !== 0 && i !== 6) rect(wallWidth + (i*horizontalLineGap) - highLookoutWidth/2, height - wallWidth, highLookoutWidth, wallWidth)
-  }
-  // Horizontal lines
-  for (var i = 0 ; i < 5 ; i++) {
-    line(0, wallWidth + (i*verticalLineGap), canvasWidth, wallWidth + (i*verticalLineGap));
-  }
+const startWidth = 225;
+var bot;
 
-  const startWidth = 225;
-  fill('#e0e1dd')
-  // start position
-  square(width/2 - startWidth/2, 0, startWidth);
+function setup() {
+  window.canvas = createCanvas(canvasWidth, canvasHeight);
+  window.canvas.parent("canvas");
+
+  angleMode(DEGREES);
+
+  bot = new Robot(wallWidth, horizontalLineGap);
 }
 
-class Robot {
-  constructor() {
-    this.x = wallWidth + 3*horizontalLineGap; 
-    this.y = wallWidth;
+function draw() {
+  drawArena();
+  bot.update();
+  bot.show();
+}
+
+function keyPressed() {
+  if (keyCode === 65) { // a 
+    bot.dir(-1, 0);
   }
-};
+  else if (keyCode === 68) { // d
+    bot.dir(1, 0);
+  }
+  else if (keyCode === 87) { // w
+    bot.dir(0, -1);
+  }
+  else if (keyCode === 83) { // s
+    bot.dir(0, 1);
+  }
+}
+
+function drawArena() {
+  background('#1b263b');
+
+  stroke('#e0e1dd');
+  strokeWeight(5);
+  fill('#415A77')
+  // Vertical lines
+  for (var i = 0; i < 7; i++) {
+    line(wallWidth + (i * horizontalLineGap), 0, wallWidth + (i * horizontalLineGap), canvasHeight);
+    // Lookout rectangle
+    if (i !== 0 && i !== 6) rect(wallWidth + (i * horizontalLineGap) - highLookoutWidth / 2, height - wallWidth, highLookoutWidth, wallWidth)
+  }
+  // Horizontal lines
+  for (var i = 0; i < 5; i++) {
+    line(0, wallWidth + (i * verticalLineGap), canvasWidth, wallWidth + (i * verticalLineGap));
+  }
+
+  noStroke();
+  fill('#e0e1dd')
+  // start position
+  square(width / 2 - startWidth / 2, 0, startWidth);
+
+}
+
