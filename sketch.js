b3a6f181d9970c31e2b09363479a3b5f270efad6
oihv <kokolas16@gmail.com>
2025-03-24 18:17:54 +0800

    Move and rename variables

--- sketch.js
+++ sketch.js
@@ -6,29 +6,29 @@ function setup() {
     window.canvas.parent("canvas");
 }
 
+const wallWidth = 37.45;
+const verticalLineGap = 281;
+const horizontalLineGap = 250;
+const highLookoutWidth = 150;
 function draw() {
   background('#1b263b');
   
   stroke('#e0e1dd');
   strokeWeight(5);
-  const wallWidth = 37.45;
-  const verticalWidth = 281;
-  const horizontalWidth = 250;
-  const highLookoutWidth = 150;
   fill('#778da9')
   // Vertical lines
   for (var i = 0 ; i < 7 ; i++) {
-    line(wallWidth + (i*horizontalWidth), 0, wallWidth + (i*horizontalWidth), canvasHeight);
-    if (i !== 0 && i !== 6) rect(wallWidth + (i*horizontalWidth) - highLookoutWidth/2, height - wallWidth, highLookoutWidth, wallWidth)
+    line(wallWidth + (i*horizontalLineGap), 0, wallWidth + (i*horizontalLineGap), canvasHeight);
+    // Lookout rectangle
+    if (i !== 0 && i !== 6) rect(wallWidth + (i*horizontalLineGap) - highLookoutWidth/2, height - wallWidth, highLookoutWidth, wallWidth)
   }
   // Horizontal lines
   for (var i = 0 ; i < 5 ; i++) {
-    line(0, wallWidth + (i*verticalWidth), canvasWidth, wallWidth + (i*verticalWidth));
+    line(0, wallWidth + (i*verticalLineGap), canvasWidth, wallWidth + (i*verticalLineGap));
   }
 
   const startWidth = 225;
   fill('#e0e1dd')
   // start position
   square(width/2 - startWidth/2, 0, startWidth);
-
 }
