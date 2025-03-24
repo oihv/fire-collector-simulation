e567a41b3d40c97fae4b23f8bbc54dac3d73e3ce
oihv <kokolas16@gmail.com>
2025-03-24 23:22:14 +0800

    Moved types and language

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const isLocal = window.location.hostname === "localhost";
 
 const script = document.createElement('script');
+script.language = "javascript"
+script.type = "text/javascript"
 script.src = isLocal
   ? "node_modules/p5/lib/p5.js"
   : "https://cdn.jsdelivr.net/npm/p5@1.11.3/lib/p5.min.js"; 
-script.type = "text/javascript"
-script.language = "javascript"
 
 document.head.appendChild(script);
 
