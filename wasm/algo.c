cdc22b0816bda41abdcf7090a4c0b4cce72a29b1
oihv <kokolas16@gmail.com>
2025-04-14 23:09:35 +0800

    Finish first algorithm prototype

--- wasm/algo.c
+++ wasm/algo.c
@@ -1,8 +1,11 @@
 #include "algo.h"
 #include <stdint.h>
+#include <string.h>
 
 extern void pathFind(BotInstruction *moves, FlameColor *const map) {
   FlameColor(*matrix)[MAP_SIZE] = generate2DArray(map);
+
+  printMap(matrix);
 
   // Initialize bot struct
   Bot bot;
@@ -11,41 +14,78 @@ extern void pathFind(BotInstruction *moves, FlameColor *const map) {
     bot.storage[i] = 0;
   }
 
+  uint8_t insIndex = 0;        // Index pointing to next empty slot on array
+  uint8_t lookoutMap[5] = {0}; // Array to track availability of lookout
+  findAlternateColor(matrix, &bot, moves, &insIndex, lookoutMap);
+  findAlternateColor(matrix, &bot, moves, &insIndex, lookoutMap);
+}
+
+int findAlternateColor(FlameColor map[MAP_SIZE][MAP_SIZE], Bot *const bot,
+                       BotInstruction *moves, uint8_t *insIndex,
+                       uint8_t *lookoutMap) {
+  FlameColor mapCopy[MAP_SIZE][MAP_SIZE]; // For restoring
+  memcpy(mapCopy, map, sizeof(FlameColor) * MAP_SIZE * MAP_SIZE);
+  uint8_t found = 0;
   // TODO: divide into cases, based on the priority, later
   Node visited[MAP_SIZE][MAP_SIZE];
   Node targetPos;
   Stack prevNodes;
-  uint8_t insIndex = 0; // Index pointing to next empty slot on array
-  BFS(matrix, &bot, Blue, visited, &targetPos);
+  initStack(&prevNodes);
+
+  found = BFS(map, bot, Blue, visited, &targetPos, 0, lookoutMap);
   traceBackPath(visited, &prevNodes, targetPos);
-  generateInstruction(&prevNodes, &bot, moves, &insIndex);
-  // TODO: implement take to moveForward, relative to current position
-  // moves[insIndex++] = take;
-  // BFS(matrix, &bot, Red, visited, &targetPos);
-  // traceBackPath(visited, &prevNodes, targetPos);
-  // generateInstruction(&prevNodes, &bot, moves, &insIndex);
-  // moves[insIndex++] = take;
-  // BFS(matrix, botPos, Blue, visited, &targetPos);
-  // traceBackPath(visited, &prevNodes, targetPos);
-  // generateInstruction(&prevNodes, &bot, moves, &insIndex);
-  // moves[insIndex++] = take;
-  // BFS(matrix, botPos, Red, visited, &targetPos);
-  // traceBackPath(visited, &prevNodes, targetPos);
-  // generateInstruction(&prevNodes, &bot, moves, &insIndex);
-  // moves[insIndex++] = take;
-  moves[insIndex] = stop;
-  printf("%d\n", insIndex);
+  generateInstruction(&prevNodes, bot, moves, insIndex);
+  moves[(*insIndex)++] = take;
+  map[targetPos.x][targetPos.y] = Empty;
+
+  found = BFS(map, bot, Red, visited, &targetPos, 0, lookoutMap);
+  traceBackPath(visited, &prevNodes, targetPos);
+  generateInstruction(&prevNodes, bot, moves, insIndex);
+  moves[(*insIndex)++] = take;
+  map[targetPos.x][targetPos.y] = Empty;
+
+  found = BFS(map, bot, Blue, visited, &targetPos, 0, lookoutMap);
+  traceBackPath(visited, &prevNodes, targetPos);
+  generateInstruction(&prevNodes, bot, moves, insIndex);
+  moves[(*insIndex)++] = take;
+  map[targetPos.x][targetPos.y] = Empty;
+
+  found = BFS(map, bot, Red, visited, &targetPos, 0, lookoutMap);
+  traceBackPath(visited, &prevNodes, targetPos);
+  generateInstruction(&prevNodes, bot, moves, insIndex);
+  moves[(*insIndex)++] = take;
+  map[targetPos.x][targetPos.y] = Empty;
+
+  // Find lookout
+  found = BFS(map, bot, Red, visited, &targetPos, 1, lookoutMap);
+  traceBackPath(visited, &prevNodes, targetPos);
+  generateInstruction(&prevNodes, bot, moves, insIndex);
+  lookoutMap[targetPos.x] = 1; // Mark as found
+  // TODO: what if, we want to diagonally go here, update later
+  moves[(*insIndex)++] = movf;
+  // Put flame
+  moves[(*insIndex)++] = puth0;
+  moves[(*insIndex)++] = puth1;
+  moves[(*insIndex)++] = puth2;
+  moves[(*insIndex)++] = puth3;
+
+  moves[(*insIndex)] = stop;
+
+  // TODO: if the route is not found, restore the original map, otherwise, keep
+  // it modified
+  return found;
 }
 
 int BFS(const FlameColor map[MAP_SIZE][MAP_SIZE], Bot *const bot,
-        FlameColor target, Node visited[MAP_SIZE][MAP_SIZE], Node *targetPos) {
+        FlameColor target, Node visited[MAP_SIZE][MAP_SIZE], Node *targetPos,
+        uint8_t lookoutFlag, uint8_t *lookoutMap) {
   Queue q; // Auto variable, no need to be cleared
   Queue *qp = &q;
   initQueue(qp);
 
   initVisitedMatrix(visited); // reset the matrix
-  visited[bot->y][bot->x].x = -1,
-  visited[bot->y][bot->x].y = -1; // Mark as start
+  visited[bot->x][bot->y].x = -1,
+  visited[bot->x][bot->y].y = -1; // Mark as start
 
   pushQueue(qp, bot->x, bot->y);
 
@@ -67,19 +107,25 @@ int BFS(const FlameColor map[MAP_SIZE][MAP_SIZE], Bot *const bot,
           continue;
 
         // Check if it's flame that we don't want, i.e. an obstacle
-        if (map[neighbor.y][neighbor.x] != 0 &&
-            map[neighbor.y][neighbor.x] != target)
+        // If in lookout mode, treat all flame as obstacle
+        if ((lookoutFlag && map[neighbor.x][neighbor.y] != 0) ||
+            (map[neighbor.x][neighbor.y] != 0 &&
+             map[neighbor.x][neighbor.y] != target))
           continue;
 
         // Check visited
-        if (visited[neighbor.y][neighbor.x].x > -2)
+        if (visited[neighbor.x][neighbor.y].x > -2)
           continue;
 
         // Mark this neighbor as visited, and previous node as current
-        visited[neighbor.y][neighbor.x].x = curr.x;
-        visited[neighbor.y][neighbor.x].y = curr.y;
+        visited[neighbor.x][neighbor.y].x = curr.x;
+        visited[neighbor.x][neighbor.y].y = curr.y;
 
-        if (map[neighbor.y][neighbor.x] == target) {
+        // If the neighbor is the target
+        // Or if is in lookout mode, the neighbor is on the last row
+        // And the lookout is currently empty
+        if ((lookoutFlag && neighbor.y == 4 && lookoutMap[neighbor.x] == 0) ||
+            map[neighbor.x][neighbor.y] == target) {
           endReached = 1;
           targetPos->x = neighbor.x;
           targetPos->y = neighbor.y;
@@ -106,8 +152,10 @@ void traceBackPath(const Node visited[MAP_SIZE][MAP_SIZE], Stack *prevNodes,
   // While temp is not the start node
   while (temp.x != -1 && temp.y != -1) {
     pushStack(prevNodes, temp.x, temp.y);
-    temp = visited[temp.y][temp.x];
+    temp = visited[temp.x][temp.y];
   }
+
+  popStack(prevNodes); // To pop the targetNode
 }
 
 void generateInstruction(Stack *prevNodes, Bot *bot, BotInstruction *moves,
@@ -115,90 +163,113 @@ void generateInstruction(Stack *prevNodes, Bot *bot, BotInstruction *moves,
   while (prevNodes->size > 0) {
     Node next = popStack(prevNodes);
 
-    if (next.x == bot->x && next.y == bot->y)
-      continue;
-
     Node dir;
     dir.x = next.x - bot->x, dir.y = next.y - bot->y;
 
     // Face the target
-    // if (prevNodes->size == 0) {
-    //   // Right
-    //   if (dir.x == 1 && dir.y == 0) {
-    //     orientateBot(bot, Right, moves, insIndex);
-    //   }
-    //   // Bottom
-    //   else if (dir.x == 0 && dir.y == 1) {
-    //     orientateBot(bot, Bottom, moves, insIndex);
-    //   }
-    //   // Left
-    //   else if (dir.x == 1 && dir.y == 0) {
-    //     orientateBot(bot, Left, moves, insIndex);
-    //   }
-    //   // Top
-    //   else if (dir.x == 0 && dir.y == -1) {
-    //     orientateBot(bot, Top, moves, insIndex);
-    //   }
-    // }
-    // Right
-    if (dir.x == 1 && dir.y == 0) {
-      moves[*insIndex] = movr;
+    if (prevNodes->size == 0) {
+      // Right (from watcher pov)
+      if (dir.x == 1 && dir.y == 0) {
+        orientateBot(bot, Right, moves, insIndex);
+      }
+      // Bottom
+      else if (dir.x == 0 && dir.y == 1) {
+        orientateBot(bot, Bottom, moves, insIndex);
+      }
+      // Left
+      else if (dir.x == -1 && dir.y == 0) {
+        orientateBot(bot, Left, moves, insIndex);
+      }
+      // Back
+      else if (dir.x == 0 && dir.y == -1) {
+        orientateBot(bot, Top, moves, insIndex);
+      }
+    } else {                // Move
+      Node normalDir = dir; // Normalized dir
+      normalizeDirection(bot->orientation, &normalDir);
+      // Right
+      if (normalDir.x == -1 && normalDir.y == 0) {
+        moves[*insIndex] = movr;
+      }
+      // Forward
+      else if (normalDir.x == 0 && normalDir.y == 1) {
+        moves[*insIndex] = movf;
+      }
+      // Left
+      else if (normalDir.x == 1 && normalDir.y == 0) {
+        moves[*insIndex] = movl;
+      }
+      // Back
+      else if (normalDir.x == 0 && normalDir.y == -1) {
+        moves[*insIndex] = movb;
+      }
+      // TODO: implement diagonal movements too
     }
-    // Bottom
-    else if (dir.x == 0 && dir.y == 1) {
-      moves[*insIndex] = movb;
-    }
-    // Left
-    else if (dir.x == -1 && dir.y == 0) {
-      moves[*insIndex] = movl;
-    }
-    // Top
-    else if (dir.x == 0 && dir.y == -1) {
-      moves[*insIndex] = movt;
-    }
-    // TODO: implement diagonal movements too
 
     bot->x += dir.x, bot->y += dir.y;
 
-    printf("curr insIndex: %d\n", *insIndex);
     (*insIndex)++;
+  }
+  // moves[*insIndex] = stop;
+}
+
+void normalizeDirection(const BotOrientation orientation, Node *dir) {
+  int8_t temp = dir->x;
+  switch (orientation) {
+  case Bottom:
+    break;
+  case Right:
+    dir->x = -dir->y;
+    dir->y = temp;
+    break;
+  case Top:
+    dir->x = -dir->x;
+    dir->y = -dir->y;
+    break;
+  case Left:
+    dir->x = dir->y;
+    dir->y = -temp;
+    break;
   }
 }
 
 void orientateBot(Bot *bot, BotOrientation orientation, BotInstruction *moves,
                   uint8_t *insIndex) {
-  if (bot->orientation == orientation)
+  if (bot->orientation == orientation) {
+    (*insIndex)--; // Offset, so it don't ruin the indexing
     return;
+  }
 
   if (bot->orientation == Right) {
     if (orientation == Left)
       moves[*insIndex] = rotb;
     else if (orientation == Top)
-      moves[*insIndex] = rotr;
-    else if (orientation == Bottom)
       moves[*insIndex] = rotl;
+    else if (orientation == Bottom)
+      moves[*insIndex] = rotr;
   } else if (bot->orientation == Bottom) {
     if (orientation == Top)
       moves[*insIndex] = rotb;
     else if (orientation == Right)
-      moves[*insIndex] = rotr;
-    else if (orientation == Left)
       moves[*insIndex] = rotl;
+    else if (orientation == Left)
+      moves[*insIndex] = rotr;
   } else if (bot->orientation == Left) {
     if (orientation == Right)
       moves[*insIndex] = rotb;
     else if (orientation == Bottom)
-      moves[*insIndex] = rotr;
-    else if (orientation == Top)
       moves[*insIndex] = rotl;
+    else if (orientation == Top)
+      moves[*insIndex] = rotr;
   } else if (bot->orientation == Top) {
     if (orientation == Bottom)
       moves[*insIndex] = rotb;
     else if (orientation == Left)
-      moves[*insIndex] = rotr;
-    else if (orientation == Right)
       moves[*insIndex] = rotl;
+    else if (orientation == Right)
+      moves[*insIndex] = rotr;
   }
+  bot->orientation = orientation;
 }
 
 void initVisitedMatrix(Node matrix[MAP_SIZE][MAP_SIZE]) {
@@ -277,10 +348,36 @@ FlameColor (*generate2DArray(FlameColor *const map))[MAP_SIZE] {
   int count = 0;
   for (int i = 0; i < MAP_SIZE; i++) {
     for (int j = 0; j < MAP_SIZE; j++) {
+      // NOTE: turns out, I aleady flipped my usage from the javascript too,
+      // so no need to retranslate the code NOTE: check for later when
+      // implementing to the robot, whether the orientation of the map is
+      // correct, when inputting, manually, it should be transposed
       arr[i][j] = map[count];
       count++;
     }
   }
 
   return arr;
+}
+
+void printMap(FlameColor map[MAP_SIZE][MAP_SIZE]) {
+  FlameColor mapCopy[MAP_SIZE][MAP_SIZE];
+  memcpy(mapCopy, map, sizeof(FlameColor) * MAP_SIZE * MAP_SIZE);
+  transposeMatrix(mapCopy);
+  for (int i = 0; i < MAP_SIZE; i++) {
+    for (int j = 0; j < MAP_SIZE; j++) {
+      printf("%d ", mapCopy[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void transposeMatrix(FlameColor map[MAP_SIZE][MAP_SIZE]) {
+  for (int i = 1; i < MAP_SIZE; i++) {
+    for (int j = 0; j < i; j++) {
+      FlameColor temp = map[i][j];
+      map[i][j] = map[j][i];
+      map[j][i] = temp;
+    }
+  }
 }
--- wasm/algo.h
+++ wasm/algo.h
@@ -6,11 +6,11 @@
 #define MAP_SIZE 5
 
 typedef enum {
-  // Move right, bottom, left, top
+  // Move right, forward, left, back
   movr,
-  movb,
+  movf,
   movl,
-  movt,
+  movb,
 
   // Rotate to left, right, or back (180 degrees)
   rotl,
@@ -53,15 +53,21 @@ typedef struct {
 } Queue, Stack; // Since they use the same term
 
 extern void pathFind(BotInstruction *moves, FlameColor *const map);
+int findAlternateColor(FlameColor map[MAP_SIZE][MAP_SIZE], Bot *const bot,
+                       BotInstruction *moves, uint8_t *insIndex,
+                       uint8_t *lookoutMap);
 // TODO: technical debt here, const in this parameter doesn't really mean that
 // the map can't be changed I believe, change later
 int BFS(const FlameColor map[MAP_SIZE][MAP_SIZE], Bot *const bot,
-        FlameColor target, Node visited[MAP_SIZE][MAP_SIZE], Node *targetPos);
+        FlameColor target, Node visited[MAP_SIZE][MAP_SIZE], Node *targetPos,
+        uint8_t lookoutFlag, uint8_t *lookoutMap);
 void initVisitedMatrix(Node matrix[5][5]);
 void traceBackPath(const Node visited[MAP_SIZE][MAP_SIZE], Stack *prevNodes,
                    const Node targetPos);
 void generateInstruction(Stack *prevNodes, Bot *bot, BotInstruction *moves,
                          uint8_t *insIndex);
+// Normalize the direction the bot should take, based on the current orientation
+void normalizeDirection(const BotOrientation orientation, Node *dir);
 void orientateBot(Bot *bot, BotOrientation orientation, BotInstruction *moves,
                   uint8_t *insIndex);
 void initQueue(Queue *q);
@@ -71,3 +77,5 @@ void initStack(Stack *q);
 Node popStack(Stack *q);
 void pushStack(Stack *q, uint8_t x, uint8_t y);
 FlameColor (*generate2DArray(FlameColor *const map))[5];
+void printMap(FlameColor map[MAP_SIZE][MAP_SIZE]);
+void transposeMatrix(FlameColor map[MAP_SIZE][MAP_SIZE]);
